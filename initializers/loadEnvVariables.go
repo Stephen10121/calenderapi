ad04b4d25a06ed2b47264ab986cf997224b3ad6e
Stephen Gruzin <stephengrzn@gmail.com>
2022-12-21 21:26:24 -0800

    Setting the root path.

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -2,12 +2,17 @@ package initializers
 
 import (
 	"log"
+	"os"
+	"regexp"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
-	err := godotenv.Load("../.env")
+	projectName := regexp.MustCompile(`^(.*calenderapi)`)
+	currentWorkDirectory, _ := os.Getwd()
+	rootPath := projectName.Find([]byte(currentWorkDirectory))
+	err := godotenv.Load(string(rootPath) + `/.env`)
 
 	if err != nil {
 		log.Fatal("Error loading .env file")
