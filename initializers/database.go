f0a7c451442ab684ec3a096ad55488203e54c652
Stephen Gruzin <stephengrzn@gmail.com>
2022-12-21 21:33:12 -0800

    adding path for database.

--- initializers/database.go
+++ initializers/database.go
@@ -10,7 +10,7 @@ var DB *gorm.DB
 
 func ConnectDatabase() {
 
-	database, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	database, err := gorm.Open(sqlite.Open("/home/pi/golang/src/github.com/stephen10121/calenderapi/test.db"), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to database!")
