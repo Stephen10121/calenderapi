239bf7e7eb3970befbb8d01dd6ed4a8e99f72916
Stephen Gruzin <stephengrzn@gmail.com>
2022-12-21 21:31:46 -0800

    Giving up. just putting absolute path.

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -1,20 +1,13 @@
 package initializers
 
 import (
-	"fmt"
 	"log"
-	"os"
-	"regexp"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
-	projectName := regexp.MustCompile(`^(.*calenderapi)`)
-	currentWorkDirectory, _ := os.Getwd()
-	rootPath := projectName.Find([]byte(currentWorkDirectory))
-	fmt.Println(string(rootPath))
-	err := godotenv.Load(string(rootPath) + `/.env`)
+	err := godotenv.Load(`/home/pi/golang/src/github.com/stephen10121/calenderapi/.env`)
 
 	if err != nil {
 		log.Fatal("Error loading .env file")
